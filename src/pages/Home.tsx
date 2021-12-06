54e5e4fb2d2bc38e104f8e3147cdba3e6bb22b54
Samuel Lewis <mazuel@gmail.com>
2021-12-06 19:40:43 +1100

    Fix favicon, home page, and more details

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import {
   Card,
-  Space,
+  Col,
+  Row,
   Typography
 } from "antd";
 import React, { useCallback } from "react";
@@ -26,35 +27,50 @@ export const HomePage: React.FC = () => {
   return (
     <div className={"home-page"}>
       <Title>Arkham Horror Card Interface</Title>
-      <Space align="start" wrap>
-        <Card title="Card">
-          <CardDisplay cardId={data} />
-        </Card>
-
-        <Card title="QR Scanner" style={{ width: 300 }}>
-          <QRScanner onScan={handleScan} onError={handleScanError} />
-          <pre>{data}</pre>
-        </Card>
-        <Card title="QR Generator" style={{ width: 300 }}>
-          <Generator />
-        </Card>
-        <Card title="Info">
-          <P>
-            This is a tool designed for use in streaming{" "}
-            <L href="https://www.fantasyflightgames.com/en/products/arkham-horror-the-card-game/">
-              Arkham Horror: The Card Game
-            </L>
-            . It allows you to create QR codes for you cards (corresponding to
-            the card ID), and then scan the code to get a digital image.
-          </P>
-          <P>
-            Use this page for testing your setup, camera alignment, and for
-            generating QR codes. Use <Link to="/stream">/stream</Link> as your
-            OBS browser source. The card display area is <b>400px</b> by{" "}
-            <b>400px</b>.
-          </P>
-        </Card>
-      </Space>
+      <Row gutter={[16, 16]}>
+        <Col span={12}>
+          <Card title="Card" style={{ minWidth: "500px", minHeight: "500px" }}>
+            <CardDisplay cardId={data} />
+          </Card>
+        </Col>
+        <Col span={8}>
+          <Card title="QR Scanner">
+            <QRScanner onScan={handleScan} onError={handleScanError} />
+            <pre>{data}</pre>
+          </Card>
+        </Col>
+        <Col span={12}>
+          <Card title="QR Generator">
+            <Generator />
+          </Card>
+        </Col>
+        <Col span={12}>
+          <Card title="Info">
+            <P>
+              This is a tool designed for use in streaming{" "}
+              <L href="https://www.fantasyflightgames.com/en/products/arkham-horror-the-card-game/">
+                Arkham Horror: The Card Game
+              </L>
+              . It allows you to create QR codes for you cards (corresponding to
+              the card ID), and then scan the code to get a digital image.
+            </P>
+            <P>
+              Use this page for testing your setup, camera alignment, and for
+              generating QR codes. Use <Link to="/stream">/stream</Link> as your
+              OBS browser source. The card display area is <b>400px</b> by{" "}
+              <b>400px</b>.
+            </P>
+            <P>
+              If there are any questions, suggestions, feedback or bugs, you can
+              raise an{" "}
+              <L href="https://github.com/Samuel-Lewis/arkham-card-interface/issues">
+                issue on the GitHub repo
+              </L>
+              .
+            </P>
+          </Card>
+        </Col>
+      </Row>
     </div>
   );
 };
